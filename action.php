987fa60493e08ec6b827711c86380d191c5cec55
Aayusha Shrestha <aicastha90@gmail.com>
2016-11-15 14:27:49 +0545

    ichanged

---
+++ action.php
@@ -0,0 +1,14 @@
+<html>
+<title> HELLO </title>
+<body>
+<form action="action_page.php" method="GET">
+NAME=<input type="text" value="" name="name" placeholder="yourname">
+ADDRESS=<input type="text" value="" name="address">
+
+<input type="submit">
+
+</form>
+
+
+</body>
+</html>
---
+++ action_page.php
@@ -0,0 +1,15 @@
+<html>
+<title>action page</title>
+<body>
+	clients name : 
+	<?php echo $_GET ["name"];
+	?>
+	<br>
+	clients address :
+	<?php echo $_GET ["address"];
+	?>
+	<?php echo $_GET ["phone no."];
+	?>
+
+</body>
+</html>
